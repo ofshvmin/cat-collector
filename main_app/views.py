10423a4b593181dfab389839a8004f4ae3e57762
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-09-02 17:34:47 -0400

    add Feeding display to details template

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
-
+from .forms import FeedingForm
 
 def home(request):
   return render(request, 'home.html')
@@ -15,7 +15,9 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', { 'cat': cat })
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form })
+  
 
 class CatCreate(CreateView):
   model = Cat
