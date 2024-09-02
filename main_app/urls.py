81070b82ccdb39c5f65231f8acfba8f70984e64e
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-09-02 10:48:55 -0400

    add a form to create a cat

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cats/', views.cat_index, name='cat-index'),
-  path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail')
+  path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+  path('cats/create/', views.CatCreate.as_view(), name='cat-create')
 ]
