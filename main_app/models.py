2ec863cd5b353470ca15a71a18b1747e13c5e5d4
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-09-04 09:06:59 -0400

    implement functionality to associate toys with cats

--- main_app/models.py
+++ main_app/models.py
@@ -25,7 +25,7 @@ class Cat(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
-  toy = models.ManyToManyField(Toy)
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
